bf1f8ef578a94a778ad3f976c8072d8f3ff81f1c
amit894 <amit.894@gmail.com>
2021-05-23 21:12:12 +0530

    Adding deployer code

---
+++ src/deployer.py
@@ -0,0 +1,12 @@
+import os
+import Jinja2
+
+def excute_rgate(args):
+    create_charts("../config.yaml")
+    run_charts()
+
+def create_charts(config):
+    print(config)
+
+def run_charts(config):
+    print(config)
