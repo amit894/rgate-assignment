bf01449664df5048b0ff2f13b3de4123cc290c27
amit894 <amit.894@gmail.com>
2021-05-24 14:15:36 +0530

    Modularization of the code

--- src/deployer.py
+++ src/deployer.py
@@ -4,57 +4,60 @@ import json
 import jinja2
 from jinja2 import Template
 
-ingress_file_path="../resources/templates-jinja/ingress/ingress.yaml"
-config_file_path="../config.yaml"
 ingress_master_dict="{'apiVersion': 'networking.k8s.io/v1', 'kind': 'Ingress', 'metadata': {'name': 'rgate', 'annotations': {'nginx.ingress.kubernetes.io/rewrite-target': '/$3'}}, 'spec': {'rules': [{'host': 'rgate', 'http': {'paths': 'None'}}]}}"
 ingress_master_dict = ingress_master_dict.replace("'", "\"")
 ingress_master_dict= json.loads(ingress_master_dict)
 
+class Deployer:
+    def __init__(self,config):
+        self.config_file_path=config
+        self.template_path="../resources/templates-jinja/"
+        self.helm_path="../resources/helm/charts/"
 
-def execute_rgate():
-    file = open(config_file_path)
-    input_list = yaml.load(file, Loader=yaml.FullLoader)
-    routes=input_list["routes"]
-    backend=input_list["backends"]
-    file.close()
+    def deploy_rgate(self):
+        file = open(self.config_file_path)
+        input_list = yaml.load(file, Loader=yaml.FullLoader)
+        routes=input_list["routes"]
+        backend=input_list["backends"]
+        file.close()
 
-    create_ingress(routes)
-    create_service(backend)
+        self.create_ingress(routes)
+        self.create_service(backend)
+
+    def create_ingress(self,routes):
+        temp_list=[]
+        ingress_file = open(self.template_path+"ingress/ingress.yaml","r+")
+        route_list=[]
+        for route in routes:
+            temp_dict={}
+            temp_port={}
+            temp_service={}
+            temp_backend={}
+            temp_dict["path"]=route["path_prefix"]
+            temp_dict["pathType"]="Prefix"
+            temp_port["number"]=8080
+            temp_service["name"]=route["backend"]
+            temp_service["port"]=temp_port
+            temp_backend["service"]=temp_service
+            temp_dict["backend"]=temp_backend
+            route_list.append(temp_dict)
+        ingress_master_dict["spec"]["rules"][0]["http"]["paths"]=route_list
+        yaml.dump(ingress_master_dict,ingress_file)
+        ingress_file.close()
+        os.system("cp -f "+ self.template_path+"ingress/ingress.yaml " + self.helm_path+ "rgate/templates")
 
 
-def create_ingress(routes):
-    temp_list=[]
-    ingress_file = open(ingress_file_path,"r+")
-    route_list=[]
-    for route in routes:
-        temp_dict={}
-        temp_port={}
-        temp_service={}
-        temp_backend={}
-        temp_dict["path"]=route["path_prefix"]
-        temp_dict["pathType"]="Prefix"
-        temp_port["number"]=8080
-        temp_service["name"]=route["backend"]
-        temp_service["port"]=temp_port
-        temp_backend["service"]=temp_service
-        temp_dict["backend"]=temp_backend
-        route_list.append(temp_dict)
-    ingress_master_dict["spec"]["rules"][0]["http"]["paths"]=route_list
-    yaml.dump(ingress_master_dict,ingress_file)
-    ingress_file.close()
-    os.system("cp -f ../resources/templates-jinja/ingress/ingress.yaml ../resources/helm/charts/rgate/templates")
+    def create_service(self,backends):
+        for backend in backends:
+            os.system("cd "+ self.helm_path+" && helm create "+backend["name"]+"-service")
+            os.system("cp -f "+ self.template_path+"/service/deployment.yaml " +self.helm_path+backend["name"]+"-service/templates")
+            os.system("cp -f " + self.template_path+"service/service.yaml " +self.helm_path+backend["name"]+"-service/templates")
+            ingress_template=Template(open(self.template_path+"service/values.yaml").read())
+            values_content=(ingress_template.render(serviceName=backend["name"]))
+            f1=open(self.helm_path+backend["name"]+"-service/values.yaml","w")
+            f1.write(values_content)
+            f1.close()
 
+D1=Deployer("../config.yaml")
 
-def create_service(backends):
-
-    for backend in backends:
-        os.system("cd ../resources/helm/charts && helm create "+backend["name"]+"-service")
-        os.system("cp -f ../resources/templates-jinja/service/deployment.yaml ../resources/helm/charts/"+backend["name"]+"-service/templates")
-        os.system("cp -f ../resources/templates-jinja/service/service.yaml ../resources/helm/charts/"+backend["name"]+"-service/templates")
-        ingress_template=Template(open('../resources/templates-jinja/service/values.yaml').read())
-        values_content=(ingress_template.render(serviceName=backend["name"]))
-        f1=open("../resources/helm/charts/"+backend["name"]+"-service/values.yaml","w")
-        f1.write(values_content)
-        f1.close()
-
-execute_rgate()
+D1.deploy_rgate()
