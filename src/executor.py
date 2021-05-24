fb022b141e6b04d024a45bca33fac2b19c843aef
amit894 <amit.894@gmail.com>
2021-05-24 14:51:15 +0530

    Separate Destroy Execution

--- src/executor.py
+++ src/executor.py
@@ -10,11 +10,5 @@ class Executor:
         for chart in charts:
             os.system("helm install --set name="+chart+" "+chart+" "+self.helm_path+chart)
 
-    def stop_rgate(self):
-        charts=os.popen("ls "+self.helm_path).read()
-        charts=charts.split()
-        for chart in charts:
-            os.system("helm uninstall "+chart)
-
 E1=Executor()
 E1.run_rgate()
